e4d62e449eb5d3c6a6c56ccf48fe4907355ba9b8
clearsky77 <clearsky3264@gmail.com>
2021-11-08 12:48:22 +0900

    필요 프래그먼트 추가

--- app/src/main/java/com/clearsky77/deliveryorder_20211108/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/clearsky77/deliveryorder_20211108/fragments/MyProfileFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.clearsky77.deliveryorder_20211108.R
 
-class PizzaStoreListFragment : Fragment(){
+class MyProfileFragment : Fragment(){
 
     override fun onCreateView(
         inflater: LayoutInflater,
