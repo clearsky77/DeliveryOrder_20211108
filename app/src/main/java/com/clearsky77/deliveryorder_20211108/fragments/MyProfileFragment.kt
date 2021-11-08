0d4b2b949735f4f9bc49863340f60ec2b9b53c0d
clearsky77 <clearsky3264@gmail.com>
2021-11-08 22:34:16 +0900

    닉네임 변경 누르면 -> 변경 화면으로 이동

--- app/src/main/java/com/clearsky77/deliveryorder_20211108/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/clearsky77/deliveryorder_20211108/fragments/MyProfileFragment.kt
@@ -1,13 +1,18 @@
 package com.clearsky77.deliveryorder_20211108.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.clearsky77.deliveryorder_20211108.EditNicknameActivity
 import com.clearsky77.deliveryorder_20211108.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 class MyProfileFragment : Fragment() {
+
+    val REQ_CODE_NICKNAME = 1000
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +24,13 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        btnEditNickname.setOnClickListener {
+
+            val myIntent = Intent(requireContext(), EditNicknameActivity::class.java)
+            startActivityForResult(myIntent, REQ_CODE_NICKNAME)
+
+        }
     }
 
 
