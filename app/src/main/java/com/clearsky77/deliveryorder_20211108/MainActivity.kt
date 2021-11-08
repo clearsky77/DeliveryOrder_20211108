aae14841ecbe325b83af1e17f49701edd0022c4e
clearsky77 <clearsky3264@gmail.com>
2021-11-08 12:15:31 +0900

    화면에 ViewPager와 연결(어댑터를 객체화)

--- app/src/main/java/com/clearsky77/deliveryorder_20211108/MainActivity.kt
+++ app/src/main/java/com/clearsky77/deliveryorder_20211108/MainActivity.kt
@@ -2,10 +2,28 @@ package com.clearsky77.deliveryorder_20211108
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clearsky77.deliveryorder_20211108.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+
     }
+
+    private fun setupEvents() {
+
+    }
+
+    private fun setValues() {
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mvpa
+    }
+
 }
