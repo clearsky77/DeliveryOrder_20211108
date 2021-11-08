e6211cf71e27a9795523a04ee525f0f5033c1a89
clearsky77 <clearsky3264@gmail.com>
2021-11-08 21:59:22 +0900

    리스트뷰에 뿌려질 데이터 / UI 태그 소환

--- app/src/main/java/com/clearsky77/deliveryorder_20211108/adapters/PizzaStoreAdapater.kt
+++ app/src/main/java/com/clearsky77/deliveryorder_20211108/adapters/PizzaStoreAdapater.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.clearsky77.deliveryorder_20211108.R
 import com.clearsky77.deliveryorder_20211108.datas.StoreData
 
@@ -23,6 +25,9 @@ class PizzaStoreAdapater(
         val row = tempRow!!
 
         val data = mList[position]
+
+        val imgLogo = row.findViewById<ImageView>(R.id.imgLogo)
+        val txtStoreName = row.findViewById<TextView>(R.id.txtStoreName)
 
         return row
 
