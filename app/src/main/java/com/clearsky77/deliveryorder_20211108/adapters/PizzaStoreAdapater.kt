7eaf1ff2e36900e251fd0c6d67e12103a1fc65bc
clearsky77 <clearsky3264@gmail.com>
2021-11-08 14:47:40 +0900

    피자가게 어댑터 기초 작업 중

--- app/src/main/java/com/clearsky77/deliveryorder_20211108/adapters/PizzaStoreAdapater.kt
+++ app/src/main/java/com/clearsky77/deliveryorder_20211108/adapters/PizzaStoreAdapater.kt
@@ -1,6 +1,12 @@
 package com.clearsky77.deliveryorder_20211108.adapters
 
+import android.content.Context
+import android.widget.ArrayAdapter
 import com.clearsky77.deliveryorder_20211108.datas.StoreData
 
-class PizzaStoreAdapater : ArrayList<StoreData>(){
+class PizzaStoreAdapater(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StoreData>
+) : ArrayAdapter<StoreData>(mContext, resId, mList){
 }
